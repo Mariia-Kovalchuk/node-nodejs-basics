0e5330fc07c355b0b096586d6fb09768fd11f3a8
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2022-06-05 19:55:03 +0300

    feat: implement cli functions

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,11 @@
-export const parseEnv = () => {
-    // Write your code here 
-};
+import { env } from 'process';
+
+// env.RSS_name1 = "value1"
+// env.RSS_name2 = "value2"
+
+export const parseEnv = (() => {
+    const filter="RSS_"
+    const variables = Object.keys(env).filter(el=>el.includes(filter))
+    variables.length ?  variables.map(el => console.log(`${el} = ${env[el]};`)) : console.log(`There is no variables with prefix  ${filter} in env`);
+
+})();
