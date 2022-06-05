ace287282f400d6bc310136a65b7d3bdf9eba2a7
Mariia Kovalchuk <mariiakovalchuk.ua@gmail.com>
2022-06-05 03:53:04 +0300

    feat: implement fs functions

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,17 @@
-export const copy = async () => {
-    // Write your code here 
-};
+import { cp } from 'fs/promises';
+import { readdir } from 'fs/promises';
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const prevDirName = 'files'
+const newDirName = 'files_copy'
+
+const filesFolderPath = path.join(dirname(fileURLToPath(import.meta.url)), prevDirName);
+const newFilesFolderPath = path.join(dirname(fileURLToPath(import.meta.url)), newDirName);
+
+export const copy = (async () => {
+    await cp(filesFolderPath, newFilesFolderPath)
+
+})();
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,30 @@
-export const read = async () => {
-    // Write your code here 
-};
+import { readFile } from 'fs/promises';
+import { readdir } from 'fs/promises';
+
+
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const filesFolderPath = path.join(dirname(fileURLToPath(import.meta.url)), 'files');
+const fileToReadPath = path.join(filesFolderPath, 'fileToRead.txt');
+
+
+
+export const read = (async () => {
+
+    try {
+        const files = await readdir(filesFolderPath);
+
+        if (files.includes('fileToRead.txt')) {
+            const content = await readFile(fileToReadPath, { encoding: 'utf8' });
+            console.log(content);
+        } else {
+            throw new Error("FS operation failed. There's no file fileToRead.txt")
+        }
+    } catch (err) {
+        console.log(err.name + ': ' + err.message);
+    }
+
+
+})();
